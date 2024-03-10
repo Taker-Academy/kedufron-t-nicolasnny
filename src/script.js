c0648ee7f73da70e450bba6e25e8a6aba0ec4e04
nicolasnny <nicolas.nunney@gmail.com>
2024-03-10 22:58:39 +0100

    [fix]: API usage with images names description and prices

--- src/script.js
+++ src/script.js
@@ -1,93 +1,78 @@
-const openMenuBtn = document.getElementById('open-basket-button');
-const closeMenuBtn = document.getElementById('close-basket-button');
-const sideMenu = document.getElementById('sideMenu');
-const add_omnia_btn = document.getElementById("add-omnia-btn");
+import { updateCartDisplay, saveCartToLocalStorage } from './cart.js';
+document.addEventListener('DOMContentLoaded', function() {
+  const itemsContainer = document.getElementById('items');
+  const cartContainer = document.querySelector('.cart');
+  const cartBtn = document.getElementById('cart-button');
+  const checkoutBtn = document.getElementById('checkoutBtn');
+  const confirmationContainer = document.getElementById('confirmation');
+  const confirmationText = document.getElementById('confirmationText');
+  let cartItems = [];
 
-let shop_items = {
-  "Omnia": { item_name: "Omnia", item_quantity: 0, item_image: "../assets/omnia.jpeg" }
-};
-
-let cart_item_list = document.getElementById("cart-items-list");
-
-function check_quantity() {
-  for (let item in shop_items) {
-    if (shop_items[item].item_quantity == 0) {
-      delete shop_items[item];
-    }
-  }
-}
-
-function create_new_item(item_name) {
-  let new_item = document.createElement("li");
-  let itemContainer = document.createElement("div");
-  itemContainer.classList.add("item-container");
-  let itemName = document.createElement("div");
-  itemName.textContent = item_name;
-  itemName.style.color = "black";
-  let new_image = document.createElement("img");
-  new_image.src = shop_items[item_name].item_image;
-  itemContainer.appendChild(itemName);
-  itemContainer.appendChild(new_image);
-  new_item.appendChild(itemContainer);
-  new_item.classList.add("cart-item");
-  return new_item;
-}
-
-function add_new_item(item_name) {
-  let new_item = create_new_item(item_name);
-  let itemQuantity = document.createElement("div");
-  itemQuantity.textContent = `Quantité: ${shop_items[item_name].item_quantity}`;
-  itemQuantity.style.color = "black";
-  let increaseButton = document.createElement("button");
-  increaseButton.textContent = "+";
-  increaseButton.addEventListener("click", () => {
-    shop_items[item_name].item_quantity++;
-    itemQuantity.textContent = `Quantité: ${shop_items[item_name].item_quantity}`;
-    localStorage.setItem(item_name, shop_items[item_name].item_quantity);
-  });
-  let decreaseButton = document.createElement("button");
-  decreaseButton.textContent = "-";
-  decreaseButton.addEventListener("click", () => {
-    if (shop_items[item_name].item_quantity > 0) {
-      shop_items[item_name].item_quantity--;
-      itemQuantity.textContent = `Quantité: ${shop_items[item_name].item_quantity}`;
-      localStorage.setItem(item_name, shop_items[item_name].item_quantity);
-      check_quantity();
+  if (localStorage.getItem('cartItems')) {
+      cartItems = JSON.parse(localStorage.getItem('cartItems'));
       updateCartDisplay();
-    }
+  }
+
+  function fetchItems() {
+      fetch('https://api.kedufront.juniortaker.com/item/')
+          .then(response => response.json())
+          .then(data => {
+              data.forEach(item => {
+                  itemsContainer.appendChild(createItemElement(item));
+              });
+          })
+  }
+
+  function createItemElement(item) {
+      const itemDiv = document.createElement('div');
+      itemDiv.classList.add('item');
+      const img = document.createElement('img');
+      img.src = `https://api.kedufront.juniortaker.com/static/img/${item.image}.png`;
+      img.alt = item.name;
+      const name = document.createElement('h3');
+      name.textContent = item.name;
+      const price = document.createElement('p');
+      price.textContent = 'Prix: ' + item.price + ' €';
+      itemDiv.addEventListener('click', () => {
+          window.location.href = `item.html?id=${item._id}`;
+      });
+      itemDiv.appendChild(img);
+      itemDiv.appendChild(name);
+      itemDiv.appendChild(price);
+      return itemDiv;
+  }
+
+  checkoutBtn.addEventListener('click', () => {
+      if (cartItems.length === 0) {
+          alert('Votre panier est vide.');
+          return;
+      }
+      window.location.href = "formulaire.html";
+      const orderData = {
+          email: 'client@example.com',
+          name: 'Client Nom',
+          address: 'Adresse de Livraison',
+          cart: cartItems.map(item => ({ id: item._id, amount: item.quantity }))
+      };
+      fetch('https://api.kedufront.juniortaker.com/order/', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(orderData)
+      })
+          .then(response => response.json())
+          .then(data => {
+              showConfirmation(data.command_id);
+              cartItems = [];
+              updateCartDisplay();
+              saveCartToLocalStorage();
+          })
   });
-  new_item.appendChild(itemQuantity);
-  new_item.appendChild(increaseButton);
-  new_item.appendChild(decreaseButton);
-  cart_item_list.appendChild(new_item);
-}
 
-function update_item(item_name) {
-  if (!shop_items[item_name]) {
-    shop_items[item_name] = { item_name: item_name, item_quantity: 0, item_image: "../assets/omnia.jpeg" };
+  function showConfirmation(commandId) {
+      confirmationText.textContent = `Votre commande a été passée avec succès. ID de Commande: ${commandId}`;
+      confirmationContainer.style.display = 'block';
   }
-  shop_items[item_name].item_quantity++;
-  localStorage.setItem(item_name, shop_items[item_name].item_quantity);
-  updateCartDisplay();
-}
-
-function updateCartDisplay() {
-  cart_item_list.innerHTML = '';
-  for (let item in shop_items) {
-    add_new_item(shop_items[item].item_name);
-  }
-}
-
-openMenuBtn.addEventListener('click', () => {
-  sideMenu.style.width = '100%';
-});
-
-closeMenuBtn.addEventListener('click', () => {
-  sideMenu.style.width = '0';
-});
-
-add_omnia_btn.addEventListener('click', () => {
-  sideMenu.style.width = '100%';
-  openMenuBtn.click();
-  update_item("Omnia");
+  fetchItems();
 });
